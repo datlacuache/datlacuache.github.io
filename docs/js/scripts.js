3824f93e34ebc4c52cf1892dd299428385d04eeb
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2022-03-10 01:37:52 -0600

    Updated particles.js. Added Google Fonts.

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -10,7 +10,7 @@
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', './js/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
+    console.log('Particles.js loaded successfully!');
 });
 
 
