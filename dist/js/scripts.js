30625e74ebe48d8850948917b49c9b1cde1013f6
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2022-03-09 21:04:15 -0600

    Initial commit. :fire:

---
+++ dist/js/scripts.js
@@ -0,0 +1,24 @@
+/*!
+* Start Bootstrap - Datlacuache Website v1.0 (undefined)
+* Copyright 2013-2022 undefined
+* Licensed under undefined (https://github.com/StartBootstrap/datlacuache.github.io/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// DOM HTML Back-end
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.get('/nosotros', (req, res) => {
+    res.sendFile('about.html', { root: __dirname });
+});
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
---
+++ src/js/scripts.js
@@ -0,0 +1,19 @@
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// DOM HTML Back-end
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.get('/nosotros', (req, res) => {
+    res.sendFile('about.html', { root: __dirname });
+});
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
